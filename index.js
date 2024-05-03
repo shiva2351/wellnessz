b6434139634b389ff32683dbdf039aef63d41b8c
shiva <smpgr2351@gmail.com>
2024-05-03 07:15:57 +0530

    3

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
 const express = require("express");
 const app = express();
-app.use(express.json());
 
-//console.log(format(new Date(), "yyyy - mm - dd"));
-const initilizeDBandServer = async () => {
-  try {
-    app.listen(3000, () => {
-      console.log("Server is running at http://localhost:3000");
-    });
-  } catch (e) {
-    console.log(`DB Error : ${e.message}`);
-    process.exit(1);
-  }
-};
-initilizeDBandServer();
+app.listen(3000, () => {
+    console.log("Server is running at http://localhost:3000");
+  });
