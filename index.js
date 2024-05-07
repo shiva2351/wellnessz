0673cac651091ca23f087a3e2dbf315c926d751b
shiva <smpgr2351@gmail.com>
2024-05-07 08:16:03 +0530

    4c

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
+app.use(cors)
 
 app.listen(3000, () => {
     console.log("Server is running at http://localhost:3000");
