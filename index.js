4a87bf5049f9a0b6ba436333991909bba812fbf8
shiva <smpgr2351@gmail.com>
2024-05-07 08:21:00 +0530

    4g

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const cors = require("cors");
 
+let tasks = [{name:"shiva",id:1}];
 const app = express();
 app.use(cors)
 
 app.listen(3000, () => {
     console.log("Server is running at http://localhost:3000");
   });
+
+  app.get('/tasks', (req, res) => {
+    console.log("get1")
+    res.json(tasks);
+});
